589679092358bcb83a5233702a19a8f4de2d2def
Zheng <aheze@getfind.app>
2023-02-22 09:10:20 -0800

    Add example

---
+++ Example/SettingExample/SettingExample/SettingExampleApp.swift
@@ -0,0 +1,19 @@
+//
+//  SettingExampleApp.swift
+//  SettingExample
+//
+//  Created by A. Zheng (github.com/aheze) on 2/22/23.
+//  Copyright © 2023 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct SettingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
